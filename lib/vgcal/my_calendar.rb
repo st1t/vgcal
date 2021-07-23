13a5bd0fe1c98a6dc1f858e63b002e504b986875
Shota Ito <shota.ito.jp@gmail.com>
2021-07-24 01:43:47 +0900

    Add all_day_event? method

--- lib/vgcal/my_calendar.rb
+++ lib/vgcal/my_calendar.rb
@@ -13,7 +13,11 @@ class MyCalendar
     tasks = my_tasks(events)
     puts "My tasks: #{tasks[0]}h(#{(tasks[0] / 8).round(2)}day)"
     tasks[1].each do |task|
-      puts "  ・#{task[0]}: #{task[1]}h"
+      if task[1].kind_of?(String)
+        puts "  ・#{task[0]}: #{task[1]}"
+      else
+        puts "  ・#{task[0]}: #{task[1]}h"
+      end
     end
     puts
     # 招待されて出席にしているタスク一覧を表示
@@ -38,29 +42,37 @@ class MyCalendar
     ["__"]
   end
 
+  def all_day_event?(date)
+    # The date, in the format "yyyy-mm-dd", if this is an all-day event.
+    # https://developers.google.com/calendar/api/v3/reference/events
+    !!/[0-9]...-[0-9].-[0-9]./.match(date.to_s)
+  end
+
   # return: [4.25, [["daily scrum", 0.25], ["SRE定例", 1.0], ["test", 2.0], ["task2", 1.0]]]
   def my_tasks(events)
     total_meeting_times = 0
     tasks_hash = {}
 
     events.items.each do |e|
-      if e.organizer.email.include?(my_email_address)
-        unless e.summary.start_with?(*hide_words)
+      if e.organizer.email.include?(my_email_address) && !e.summary.start_with?(*hide_words)
+        if all_day_event?(e.start.date)
+          tasks_hash[e.summary] = 'all day'
+        else
           task_time = (e.end.date_time - e.start.date_time).to_f * 24
+          # 1日の中で同じタスク名が複数あったら時間を集計する。例.午前と午後それぞれでAプロジェクトに1時間スケジュール
           if tasks_hash[e.summary]
             tasks_hash[e.summary] += task_time
           else
             tasks_hash[e.summary] = task_time
           end
-
           total_meeting_times = 0
           tasks_hash.each do |h|
-            total_meeting_times += h.to_ary[1]
+            total_meeting_times += h.to_ary[1] unless h.to_ary[1].kind_of?(String)
           end
         end
       end
     end
-    [total_meeting_times, tasks_hash.to_a]
+    [total_meeting_times, tasks_hash.sort.to_a]
   end
 
   # return: [2.25, [["朝会", 0.5], ["daily scrum", 0.25], ["SRE朝会", 0.5], ["定例", 1.0]]]
@@ -93,7 +105,7 @@ class MyCalendar
         end
       end
     end
-    [total_meeting_times, tasks_hash.to_a]
+    [total_meeting_times, tasks_hash.sort.to_a]
   end
 
   def calendar_events
